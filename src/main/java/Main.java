ccef9678e28a29eea7eaa9aab6d08afa96371f20
omergiladi <124587360+omergiladi@users.noreply.github.com>
2025-12-23 17:59:35 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-class Main {
-    public static Node<Integer> sortLinkedList(Node<Integer> head1, Node<Integer> head2) {
+public static Node<Integer> sortLinkedList(Node<Integer> head1, Node<Integer> head2) {
         Node<Integer> plough = new Node<Integer>(-1,null);
         Node<Integer> rike = plough;
         Node<Integer> p1 = head1;
@@ -48,33 +47,56 @@ class Main {
 
     
     //Big O of O(n^2).
-}
-
-public static int disFromEdges(Node<Integer> head, int num) {
-    boolean hasAppeared = false;
-    int startCounter = 0;
-    int endCounter = 0;
-    while (head != null) {
-        if (head.getValue() == num) {
-            hasAppeared = true;
-            endCounter = 0;
+    
+    public static int disFromEdges(Node<Integer> head, int num) {
+        boolean hasAppeared = false;
+        int startCounter = 0;
+        int endCounter = 0;
+        while (head != null) {
+            if (head.getValue() == num) {
+                hasAppeared = true;
+                endCounter = 0;
+            }
+            if (hasAppeared == false && head.getValue() != num) {
+                startCounter+=1;
+            }
+            if (hasAppeared == true && head.getValue() != num) {
+                endCounter+=1;
+            }
+            head = head.getNext();
         }
-        if (hasAppeared == false && head.getValue() != num) {
-            startCounter+=1;
-        }
-        if (hasAppeared == true && head.getValue() != num) {
-            endCounter+=1;
-        }
-        head = head.getNext();
+        if (!hasAppeared) return -1;
+        return (startCounter + endCounter);
     }
-    if (!hasAppeared) return -1;
-    return (startCounter + endCounter);
-}
+    
+    //Big O of O(n).
+    
+    public static boolean allUnique(Node<Integer> head) {
+    Node<Integer> cur = head;
 
-public static boolean allUnique(Node<Integer> head) {
-    while (head.hasNext()) {
-        if (head.getValue() != head.getNext().getValue()) return false;
-        head = head.getNext();
+    while (cur != null) {
+        Node<Integer> nxt = cur.getNext();
+        while (nxt != null) {
+            if (cur.getValue().equals(nxt.getValue())) return false;
+            nxt = nxt.getNext();
+        }
+        cur = cur.getNext();
     }
+
     return true;
 }
+    
+    public static Node<Integer> newUnique(Node<Integer> head) {
+        Node<Integer> dummy = new Node<>(-1, null);
+        Node<Integer> first = dummy;
+    
+        while (head != null) {
+            if (!contains(dummy.getNext(), head.getValue())) {
+                first.setNext(new Node<>(head.getValue(), null));
+                first = first.getNext();
+            }
+            head = head.getNext();
+        }
+    
+        return dummy.getNext();
+    }
